d5bcf59a44af900dbf72b2343991122e4a304ef4
Isaac Chen <tingyi364@gmail.com>
2019-08-13 04:15:54 +0200

    RMX1911: Add BluetoothResCommon
    Change-Id: Id687d37d41d2f3918d47ba2c5180a73dabe0fd9c

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    BluetoothResCommon
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
