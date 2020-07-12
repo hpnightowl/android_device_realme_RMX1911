a7e9916205f64cb210e1841103844a0f307e260a
hpnightowl <iamhp2k@gmail.com>
2020-07-12 09:41:49 +0530

    RMX1911:inherit common properties

--- device.mk
+++ device.mk
@@ -1,7 +1,14 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# AID/fs configs
+# Inherit vendor
+$(call inherit-product, vendor/realme/RMX1911/RMX1911-vendor.mk)
+
 PRODUCT_PACKAGES += \
     fs_config_files
 
@@ -254,5 +261,3 @@ PRODUCT_COPY_FILES += \
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
-# Inherit vendor
-$(call inherit-product, vendor/realme/RMX1911/RMX1911-vendor.mk)
