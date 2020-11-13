c4ba22660f959ee47d217e86dcbbeddd5d9735bc
HARSH <iamhp2k@gmail.com>
2020-11-13 12:04:50 +0530

    RMX1911: owls aosp bringup

--- aosp_RMX1911.mk
+++ aosp_RMX1911.mk
@@ -5,7 +5,7 @@
 #
 
 $(call inherit-product, device/realme/RMX1911/device.mk)
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Device identifier. This must come after all inclusions.
