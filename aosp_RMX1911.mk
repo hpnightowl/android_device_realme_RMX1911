4c41f89f97e35be4c9951d62d377c6adc1c55e83
HARSH <iamhp2k@gmail.com>
2020-11-13 12:06:36 +0530

    RM1911: GMS client realme
    *according to stock now it is realme but when it was pie build it was  oppo

--- aosp_RMX1911.mk
+++ aosp_RMX1911.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 PRODUCT_NAME := aosp_RMX1911
 PRODUCT_DEVICE := RMX1911
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := realme 5
+PRODUCT_MODEL := Realme 5
 PRODUCT_MANUFACTURER := realme
 
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
+PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 # Fingerprint
 BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
